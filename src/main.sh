ee3d1bd47d357e05b0047446faa681c6c215da05
Sumudu Lansakara <sumudu.lansakara@gmail.com>
2017-07-19 08:40:34 +0530

    super main script argparse implemented
    Signed-off-by: Sumudu Lansakara <sumudu.lansakara@gmail.com>

--- src/main.sh
+++ src/main.sh
@@ -3,14 +3,63 @@ function __init__(){
     export ROOT="$(dirname $0)"
     source $ROOT/settings.sh
 
-    if [[ $# -eq 0 ]]; then
-        echo "[DBG] running in debug mode"
+    __argparse__ $*
+
+    if [[ -z $INSTANCEID ]]; then
+        if ! $DEBUG_MODE; then
+            echo "[ERR][MAIN] instance ID not provided"
+            exit 1
+        fi
         export INSTANCEID="$(date +%Y%M%d%H%m%S%3N)"
-    else
-        export INSTANCEID="$1"
+        echo "[DBG] starting debug instance: $INSTANCEID"
     fi
 
     source $ROOT/terminal/main.sh
+}
+
+function __argparse__(){
+    getopt --test >/dev/null
+    if [[ $? -ne 4 ]]; then
+        echo "[ERR][MAIN] unable to parse arguments"
+        exit 1
+    fi
+
+    # id, cmd, debug
+    SHORT_ARGS=i:c:d
+
+    PARSED=$(getopt --options $SHORT_ARGS --name "$0" -- "$@")
+    if [[ $? -ne 0 ]]; then
+        echo "[ERR][MAIN] invalid arguments"
+        exit 1
+    fi
+
+    eval set -- "$PARSED"
+
+    export DEBUG_MODE=false
+    while true; do
+        case "$1" in
+            -i|--id)
+                export INSTANCEID="$2"
+                shift 2
+                ;;
+            -c|--cmd)
+                COMMAND=$2
+                shift 2
+                ;;
+            -d|--debug)
+                export DEBUG_MODE=true
+                shift
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo "[WRN][MAIN] invalid argument $1"
+                shift
+                ;;
+        esac
+    done
 }
 
 __init__ $*
